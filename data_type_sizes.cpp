773e4f1169cea219ba9f906659c9d97d1cbf1f98
Vinayak Dwarkoba Gaikwad <vinayakdwarkobagaikwad@gmail.com>
2021-12-26 17:42:42 +0530

    Create data_type_sizes.cpp

---
+++ data_type_sizes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int q, i;
+  bool flag = false;
+
+  cout << "Size of char:" << sizeof(char);
+  cout << "\nSize of int:" << sizeof(int);
+  cout << "\nSize of floa:t" << sizeof(float);
+  cout << "\nSize of double:" << sizeof(double);
+  
+  return 0;
+}
