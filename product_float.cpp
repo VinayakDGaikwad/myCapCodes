bd129da9823be768793e64806e6938d1404b23ab
Vinayak Dwarkoba Gaikwad <vinayakdwarkobagaikwad@gmail.com>
2021-12-26 17:45:54 +0530

    Create product_float.cpp

---
+++ product_float.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  float a, b;
+  cout << "Enter two numbers:";
+  cin >> a;
+  cin >> b;
+
+  cout << "Product is:" << a * b;
+
+  return 0;
+}
